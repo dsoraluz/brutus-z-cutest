3a34bdf7b70075eba4f941a55810edd49a78b6ed
dsoraluz <d.soraluz@gmail.com>
2017-06-05 01:44:16 -0400

    nodemailer and dotenv added, added .gitignore, need to configure nodemailer to send to brutus@brutuszcutest.com

--- routes/site-routes.js
+++ routes/site-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const siteRoutes = express.Router();
 
+const nodemailer = require('nodemailer');
+
 siteRoutes.get('/about', (req,res,next)=>{
   res.render('about');
 });
@@ -19,6 +21,50 @@ siteRoutes.get('/social', (req,res,next)=>{
 
 siteRoutes.get('/contact', (req,res,next)=>{
   res.render('contact');
+});
+
+siteRoutes.post('/contact', (req,res,next)=>{
+
+  let name = req.body.name;
+  let email = req.body.email;
+
+  let userMessage = req.body.message;
+
+
+  // Create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USERNAME, //User Email address
+      pass: process.env.GMAIL_PASSWORD
+    }
+  });
+
+  // Setup email data with unicode symbols
+  let mailOptions = {
+    // Sender address
+    from: `New Soul Transformation <soultrahsformation@gmail.com>`,
+    // List of receivers
+    to: `${email}`,
+    // Subject Line
+    subject: `${toFirstName}, Welcome to New Soul Transformation!!`,
+    // Plain text body
+    html: `<h2> WELCOME! </h2> \n <p>${toFirstName}, <br/><br/> Send your friends the following link </p> \n <a href="localhost:3000/signup/${id}">  <h3>SIGNUP LINK</h3> </a>`
+  };
+
+  // Send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info)=>{
+    if (error){
+      res.send(500);
+      console.log(error);
+      return;
+    }else {
+      console.log('Message %s sent: %s', info.messageId, info.response);
+      transporter.close();
+      res.redirect('/');
+    }
+  });
+
 });
 
 
