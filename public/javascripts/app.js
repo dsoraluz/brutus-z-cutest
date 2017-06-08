e1f400c4e5d374e7d362dd85e2eeeacb1a8dc183
dsoraluz <d.soraluz@gmail.com>
2017-06-08 19:04:12 -0400

    made some styling adjustments as per client's request, debugging mobile nav bar not working error, added content to about, possible photo slider

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,58 @@
 $(document).ready(()=>{
-  $('.title').hide();
-  $('.title').fadeIn(3000,()=>{
+  $('.button-collapse').sideNav();
+
+  // $('.title').hide();
+  // $('.title').fadeIn(3000,()=>{
+  //
+  //
+  // });
+
+  $('.review-1').css('opacity', '1');
+     setInterval(rotateImages, 5000);
 
 
-  });
+    function rotateImages() {
+      let firstRev = $('.review-1');
+      let secondRev = $('.review-2');
+      let thirdRev = $('.review-3');
+
+      if (firstRev.hasClass('current')) {
+        firstRev.removeClass('current');
+          firstRev.animate({
+            opacity: 0
+          }, 1000, function() {
+            secondRev.addClass('current');
+            secondRev.animate({
+              opacity: 1
+            });
+          });
+      }
+      else if (secondRev.hasClass('current')) {
+        secondRev.removeClass('current');
+        secondRev.animate({
+          opacity: 0
+        }, 1000, function() {
+          thirdRev.addClass('current');
+          thirdRev.animate({
+            opacity: 1
+          });
+        });
+
+      }
+      else if (thirdRev.hasClass('current')){
+        thirdRev.removeClass('current');
+        thirdRev.animate({
+          opacity: 0
+        }, 1000, function() {
+          firstRev.addClass('current');
+          firstRev.animate({
+            opacity: 1
+          });
+        });
+
+      }
+
+    }
 
   // $('#message').focusin(()=>{
   //   let value = $('#message').val();
