03a4d499510fc53999f2a2f36a678408455e6895
dsoraluz <d.soraluz@gmail.com>
2017-06-18 17:52:15 -0400

    added styles and favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.locals.title = 'Brutus Z Cutest - Official Page';
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
